22e4c72d983ea4f67d0c45615b5cdd7217912a6c
Tam <hi@tam.sx>
2018-07-27 18:06:41 +0100

    Basic Craft 3 plugin setup

---
+++ src/Out.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Out for Craft 3
+ *
+ * @link      https://ethercreative.co.uk
+ * @copyright Copyright (c) Ether Creative
+ */
+
+namespace ether\out;
+
+use craft\base\Plugin;
+
+
+/**
+ * @author  Ether Creative
+ * @package ether\out
+ * @since   1.0.0
+ */
+class Out extends Plugin
+{
+
+	// Properties
+	// =========================================================================
+
+	public $schemaVersion = '1.0.0';
+	public $hasCpSettings = true;
+	public $hasCpSection  = true;
+
+}
